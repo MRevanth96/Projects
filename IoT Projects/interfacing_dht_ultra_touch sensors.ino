84df55ca66397262b5eb7e829a7934e28a5de140
Revanth <31589194+MRevanth96@users.noreply.github.com>
2018-06-22 00:05:47 +0530

    Add files via upload

---
+++ IoT Projects/interfacing_dht_ultra_touch sensors.ino
@@ -0,0 +1,71 @@
+#include <DHT.h>
+
+#define DHTPIN 5                            //Pin D1
+#define DHTTYPE DHT11
+#define TRIGGER 11 // defining trigger pin
+#define ECHO 12 // defining echo pin
+
+
+DHT dht(DHTPIN, DHTTYPE);
+int TouchSensor = 9; //connected to Digital pin D3
+int led = 2;
+
+void setup()
+{
+  Serial.begin(9600); // Communication speed
+  pinMode(led, OUTPUT);
+  pinMode(TouchSensor, INPUT);
+  pinMode(TRIGGER, OUTPUT); //initializing trigger as output
+  pinMode(ECHO, INPUT); //initialing trigger as input
+  
+}
+void loop(){
+  if(digitalRead(TouchSensor)==HIGH)       //Read Touch sensor signal
+   { 
+    digitalWrite(led, HIGH);   // if Touch sensor is HIGH, then turn on
+    Serial.println("Pressing");
+   Temp:   Serial.println("Collecting temperature data.");
+  float h = dht.readHumidity();                       // Reading humidity
+
+  float t = dht.readTemperature();                    // Read temperature as Celsius
+
+  if (isnan(h) || isnan(t))                           // Check if any reads failed and exit early (to try again).
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println("Temperature :");
+  Serial.println(t);
+
+  Serial.println("Humidity :");
+  Serial.println(h);
+  void ultrasonic();
+  delay(1000);
+   }
+  else
+   {
+    digitalWrite(led, LOW);    // if Touch sensor is LOW, then turn off the led
+    Serial.println("Press the button");
+   }
+  delay(1000); // Slow down the output for easier reading
+}
+void ultrasonic()
+{  int duration1, dist1;
+  digitalWrite(TRIGGER, LOW); // make trigger low
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER, HIGH); // make trigger high
+  delayMicroseconds(10); //give 10 microsec delay
+  digitalWrite(TRIGGER, LOW);
+  duration1 = pulseIn(ECHO, HIGH);// It gives the duration of on time in echo
+  dist1 = (duration1 / 2) / 29.1; // calibrate the distance using pulse
+  // Soundwave travel in 343m/sec it can be converted into 29.155 microsecond/cm.
+  Serial.print(dist1); // Print the distance value
+  Serial.println("distance");
+  delay(500);
+  if (dist1<10){
+    goto Temp;
+    }
+    
+  
+}
---
+++ IoT Projects/led_patterns using bits and bytes.ino
@@ -0,0 +1,43 @@
+int ledPin[] = {6,5,4,0};
+
+void setup()
+{
+  for (int i =0;i<4;i++)
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+}
+
+void loop() 
+{
+  byte nums[] = {0, 1, 3, 6, 4, 12, 8, 12, 4, 6, 3, 1, 0};
+  for (byte i = 0; i<13;i++)
+  {
+    displayBinary(nums[i]);
+    delay(1000);
+  } 
+}
+
+void displayBinary( byte numToShow)
+{
+  for (int i =0;i<4;i++)
+  {
+    if (bitRead(numToShow, i)==1)//bitRead(x,n)---: x-byte from which bit has to be read  n-the bit to read
+    {
+      digitalWrite(ledPin[i], HIGH); 
+    }
+    else
+    {
+      digitalWrite(ledPin[i], LOW); 
+    }
+  }
+}
+
+// 0-0000,1-0001,3-0011,6-0110,12-1100,8-1000,4-0100,6-0110,3-0011,1-0001,0-0000
+//Inside displaybinary function each byte can execute
+//Ex. numToShow=3---bitRead(0011,0),bitRead(0011,1),bitRead(0011,2),bitRead(0011,3)
+//0th bit=1 then 6th pin high ;1st bit=1 then 5th pin high ; 2nd bit=0 then 4th pin low;
+//3rd bit=0 then 0th pin low;
+//then delay(1000) after that it go to displayNumber function
+//OUTPUT= {(6,5),(5,4),(4,0),(0,4),(4,5),(5,0)} pins are high
+//Each set execute after a delay
